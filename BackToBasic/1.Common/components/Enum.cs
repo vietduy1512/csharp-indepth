45855e5d5e2ffd7664b7b4d1c3bba9f48f1dee38
Viết Duy Lê <Duy.LeViet1@Nashtechglobal.com>
2020-03-06 15:00:30 +0700

    Implement struct, enum

---
+++ BackToBasic/1.Common/components/Enum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1.Common.components
+{
+    enum WeekDays
+    {
+        Monday,     // value: 0
+        Tuesday,    // value: 1
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
---
+++ BackToBasic/1.Common/components/Struct.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1.Common.components
+{
+    // https://www.tutorialsteacher.com/csharp/csharp-struct
+    struct Employee
+    {
+        public int EmpId;
+        public string FirstName;
+        public string LastName;
+    }
+}
