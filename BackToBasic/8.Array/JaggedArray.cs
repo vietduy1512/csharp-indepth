62e41114c4b8a5d094d07ddaf5c53f23915a17db
Duy <vietduy1512@gmail.com>
2020-03-06 21:28:10 +0700

    Implement multidimension array and jagged array

---
+++ BackToBasic/8.Array/JaggedArray.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _8.Array
+{
+    class JaggedArray
+    {
+        public void Main()
+        {
+            // Jagged Arrays
+            int[][] jArray1 = new int[2][]; // can include two single-dimensional arrays 
+            int[][,] jArray2 = new int[3][,]; // can include three two-dimensional arrays
+
+            // Access
+            _ = jArray1[0][0];
+            _ = jArray1[1];
+        }
+    }
+}
---
+++ BackToBasic/8.Array/MultidimensionalArrays.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _8.Array
+{
+    class MultidimensionalArrays
+    {
+        public void Main()
+        {
+            // two-dimensional Array
+            int[,] arr2d1 = new int[3, 2] 
+            {
+                {1, 2},
+                {3, 4},
+                {5, 6}
+            };
+
+            // or 
+            int[,] arr2d2 = {
+                {1, 2},
+                {3, 4},
+                {5, 6}
+            };
+
+            // Access
+            _ = arr2d1[0, 0];
+        }
+    }
+}
