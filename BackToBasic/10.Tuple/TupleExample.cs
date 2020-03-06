18a29fcaff4c178090a155a65699ad460db7fdde
Duy <vietduy1512@gmail.com>
2020-03-06 21:51:12 +0700

    Implement Tuple & ValueTuple

---
+++ BackToBasic/10.Tuple/TupleExample.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _10.Tuples
+{
+    class TupleExample
+    {
+        public void Main()
+        {
+            // Tuple Initialization
+            // C1:
+            Tuple<int, string, string> person1 = new Tuple<int, string, string>(1, "Steve", "Jobs");
+
+            // C2:
+            var person2 = Tuple.Create(1, "Steve", "Jobs");
+
+            // Access
+            _ = person1.Item1;
+            _ = person1.Item2;
+            _ = person1.Item3;
+        }
+    }
+}
---
+++ BackToBasic/10.Tuple/ValueTupleExample.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _10.Tuples
+{
+    class ValueTupleExample
+    {
+        public void Main()
+        {
+            // ValueTuple Initialization
+            (int, string, string) person1 = (1, "Steve", "Jobs");
+            var person2 = (1, "Steve", "Jobs");
+
+            // Tuple requires at least two values. The following is NOT a tuple
+            var number = (1);  // int type, NOT a tuple
+
+            // Named Members
+            (int Id, string FirstName, string LastName) person = (1, "Bill", "Gates");
+            _ = person.Id;   // returns 1
+            _ = person.FirstName;  // returns "Bill"
+            _ = person.LastName; // returns "Gates"
+
+            // Assign member name with values
+            var personA = (Id: 1, FirstName: "Bill", LastName: "Gates");
+            
+            // Note*
+            // PersonId, FName, LName will be ignored.
+            (int Id, string FirstName, string LastName) personB = (PersonId: 1, FName: "Bill", LName: "Gates");
+            // PersonId, FirstName, LastName will be ignored. It will have the default names: Item1, Item2, Item3.
+            //(string, string, int) personC = (PersonId: 1, FName: "Bill", LName: "Gates");
+
+            // Deconstruction
+            (int Id, string FirstName, string LastName) = personA;
+            FirstName = LastName;
+        }
+    }
+}
