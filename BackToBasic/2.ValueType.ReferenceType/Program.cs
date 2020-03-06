649de085a35a29085ccd1183ee0b9e376e5f8755
Viết Duy Lê <Duy.LeViet1@Nashtechglobal.com>
2020-03-06 14:33:42 +0700

    Implement Value & Reference Type

---
+++ BackToBasic/2.ValueType.ReferenceType/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _2.ValueType.ReferenceType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://www.tutorialsteacher.com/csharp/csharp-value-type-and-reference-type
+            // TBD
+
+            // 1. Value type stores the value in its memory space, whereas reference type stores the address of the value where it is stored.
+            // 2. Primitive data types and struct are of the 'Value' type.Class objects, string, array, delegates are reference types.
+            // 3. Value type passes byval by default. Reference type passes byref by default.
+            // 4. Value types and reference types stored in Stack and Heap in the memory depends on the scope of the variable.
+        }
+    }
+}
