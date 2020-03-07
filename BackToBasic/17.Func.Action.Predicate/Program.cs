c3130db6f29917cdd3238aabf3de06e45a6fe4e1
Duy <vietduy1512@gmail.com>
2020-03-07 17:08:45 +0700

    Implement Func, Actioon,Predicate

---
+++ BackToBasic/17.Func.Action.Predicate/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _17.Func.Action.Predicate.Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        void MainTemp()
+        {
+            // Ways to define Func
+            /// Has n input parameters and one out parameter
+            Func<int, int, int> add1 = Sum;
+            Func<int, int, int> add2 = (x, y) => x + y;
+            Func<int, int, int> add3 = delegate (int x, int y)
+            {
+                return x + y;
+            };
+
+            // Ways to define Action
+            /// Action delegate doesn't return a value
+            Action<int> act1 = SumAction;
+            Action<int> act2 = (i) => { };
+            Action<int> act3 = delegate (int i)
+            {
+                Console.WriteLine(i);
+            };
+
+            // Predicate
+            /// Predicate delegate methods must take one input parameter and return a boolean - true or false
+        }
+
+        int Sum(int x, int y)
+        {
+            return x + y;
+        }
+
+        void SumAction(int i)
+        {
+
+        }
+    }
+}
