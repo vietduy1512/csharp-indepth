b7e8be3dc0191e866d939c0d47434300529f181c
Duy <vietduy1512@gmail.com>
2020-03-07 16:31:22 +0700

    Implement anonymous method

---
+++ BackToBasic/14.AnonymousMethod/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _14.AnonymousMethod
+{
+    class Program
+    {
+        public delegate void Print(int value);
+
+        static void Main(string[] args)
+        {
+            // JS: function() {}
+            // C#: delegate() {}
+            Print print = delegate (int val) {
+                Console.WriteLine("Inside Anonymous method. Value: {0}", val);
+            };
+        }
+    }
+}
