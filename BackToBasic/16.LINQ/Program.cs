261684a5144b5b27e6fec7e212f07376b728c832
Duy <vietduy1512@gmail.com>
2020-03-07 16:57:30 +0700

    Implement basic LINQ

---
+++ BackToBasic/16.LINQ/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _16.LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int> { 1, 2, 3 };
+
+            var result1 = list.Where(x => x == 1);
+        }
+    }
+}
