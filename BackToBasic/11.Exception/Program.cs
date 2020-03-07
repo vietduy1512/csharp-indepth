2b0a80176d1fe1404c512b45c836d0f58c717d55
Duy <vietduy1512@gmail.com>
2020-03-07 14:17:08 +0700

    Implement exception

---
+++ BackToBasic/11.Exception/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _11.Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://www.tutorialsteacher.com/csharp/csharp-exception
+            // Exception Handling
+            // throw
+            // Custom Exception
+
+
+            try
+            {
+                // code that may raise exceptions
+            }
+            catch (Exception ex)
+            {
+                // handle exception
+            }
+            finally
+            {
+                // final cleanup code
+            }
+        }
+    }
+}
