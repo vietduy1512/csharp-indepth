43346c6c7464dbe3422fd72e8138035cac889bc8
Duy <vietduy1512@gmail.com>
2020-03-07 16:27:49 +0700

    Implement Event

---
+++ BackToBasic/13.Events/PrintHelper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _13.Events
+{
+    public class PrintHelper
+    {
+        // declare delegate
+        public delegate void BeforePrint();
+
+        //declare event of type delegate
+        public event BeforePrint beforePrintEvent;
+
+        public PrintHelper()
+        {
+
+        }
+
+        public void PrintNumber(int num)
+        {
+            //call delegate method before going to print
+            if (beforePrintEvent != null)
+                beforePrintEvent();
+
+            Console.WriteLine("Number: {0,-12:N0}", num);
+        }
+
+        public void PrintDecimal(int dec)
+        {
+            if (beforePrintEvent != null)
+                beforePrintEvent();
+
+            Console.WriteLine("Decimal: {0:G}", dec);
+        }
+
+        public void PrintMoney(int money)
+        {
+            if (beforePrintEvent != null)
+                beforePrintEvent();
+
+            Console.WriteLine("Money: {0:C}", money);
+        }
+
+        public void PrintTemperature(int num)
+        {
+            if (beforePrintEvent != null)
+                beforePrintEvent();
+
+            Console.WriteLine("Temperature: {0,4:N1} F", num);
+        }
+        public void PrintHexadecimal(int dec)
+        {
+            if (beforePrintEvent != null)
+                beforePrintEvent();
+
+            Console.WriteLine("Hexadecimal: {0:X}", dec);
+        }
+    }
+}
---
+++ BackToBasic/13.Events/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _13.Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        void MainTemp()
+        {
+            // Advantages: only exposes the 'add' and 'remove' methods
+            // modifying how the delegate can be accessed
+            PrintHelper _printHelper = new PrintHelper();
+            _printHelper.beforePrintEvent += DoSth;
+
+            _printHelper.PrintMoney(1);
+        }
+
+        public void DoSth()
+        {
+            Console.WriteLine("BeforPrintEventHandler: PrintHelper is going to print a value");
+        }
+    }
+}
