92aaff45a25d518aa948e988cbe7d71db3f25643
Duy <vietduy1512@gmail.com>
2020-03-07 16:41:23 +0700

    Implement null types, null-coalescing, null condition

---
+++ BackToBasic/15.Nullable.NullCoalescing/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _15.Nullable.NullCoalescing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Nullable types
+            int? i = null;      // or Nullable<int> i = null;
+
+            // Null-coalescing operator (??)
+            var result = i ?? 0;
+
+            // Null conditional operator (?.)
+            var result2 = i?.ToString();
+        }
+    }
+}
